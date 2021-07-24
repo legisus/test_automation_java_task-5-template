51114c0c42dcb384bc0873cef9102a68c3ac5437
Mykola <legisus@gmail.com>
2021-07-24 03:22:36 +0200

    1.1

--- src/main/java/com/epam/test/automation/java/practice5/Rectangle.java
+++ src/main/java/com/epam/test/automation/java/practice5/Rectangle.java
@@ -8,32 +8,23 @@ public class Rectangle {
     public Rectangle(double a, double b) {
         this.sideA = a;
         this.sideB = b;
-
-        //Implement constructor with 'a' and 'b' parameters;
-        //set sideA and sideB to specified values;
     }
     public Rectangle(double a) {
-        //Implement constructor with 'a' parameter;
-        //set sideB to default value;
         this.sideA = a;
         this.sideB = 5;
     }
 
     public Rectangle() {
-        // Implement default constructor;
-        //set sideA and sideB to default values;
         this.sideA = 4;
         this.sideB = 3;
 
     }
-
     /**
      * <summary>
      * Implement code according to description of task.
      * </summary>
      */
     public double getSideA() {
-        // Delete line below and write your own solution;
        return this.sideA;
     }
 
@@ -43,7 +34,6 @@ public class Rectangle {
      * </summary>
      */
     public double getSideB() {
-        // Delete line below and write your own solution;
         return this.sideB;
     }
 
@@ -53,7 +43,6 @@ public class Rectangle {
      * </summary>
      */
     public double area() {
-        // Delete line below and write your own solution;
         return this.sideA*this.sideB;
     }
 
@@ -63,7 +52,6 @@ public class Rectangle {
      * </summary>
      */
     public double perimeter() {
-        // Delete line below and write your own solution;
         return ((this.sideA*2)+(this.sideB*2));
     }
 
@@ -73,7 +61,6 @@ public class Rectangle {
      * </summary>
      */
     public boolean isSquare() {
-        // Delete line below and write your own solution;
         return this.sideA == this.sideB;
     }
 
@@ -83,7 +70,6 @@ public class Rectangle {
      * </summary>
      */
     public void replaceSides() {
-        // Delete line below and write your own solution;
         double c;
         c = this.sideA;
         this.sideA = this.sideB;
